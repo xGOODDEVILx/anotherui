ec3633f31389691d12922008f3e9dbb814fe4faa
Gooddevil <behroziyan.mhrn@gmail.com>
2024-11-26 19:59:14 +0330

    Implement Button compnent and its variants

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
+import { User2 } from 'lucide-react';
 
 export type Story = StoryObj<typeof Button>;
 
@@ -31,6 +32,10 @@ const meta: Meta<typeof Button> = {
       control: 'select',
       options: ['normal', 'pill'],
     },
+    mode: {
+      control: 'inline-radio',
+      options: ['default', 'icon-only'],
+    },
   },
 };
 
@@ -41,29 +46,217 @@ export const Default: Story = {
     variant: 'default',
     size: 'md',
     shape: 'normal',
+    mode: 'default',
   },
-};
-
-export const Outline: Story = {
-  args: {
-    variant: 'outline',
-    size: 'md',
-    shape: 'pill',
-  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
 };
 
 export const Danger: Story = {
   args: {
-    variant: 'outline-danger',
-    size: 'lg',
+    variant: 'danger',
+    size: 'md',
     shape: 'normal',
+    mode: 'default',
   },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
 };
 
 export const Success: Story = {
   args: {
     variant: 'success',
-    size: 'sm',
-    shape: 'pill',
+    size: 'md',
+    shape: 'normal',
+    mode: 'default',
   },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
+};
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
+};
+
+export const OutLine: Story = {
+  args: {
+    variant: 'outline',
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
+};
+
+export const OutLineDanger: Story = {
+  args: {
+    variant: 'outline-danger',
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
+};
+
+
+export const OutLineSuccess: Story = {
+  args: {
+    variant: 'outline-success',
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button {...context.args} size="sm" />
+        <Button {...context.args} size="md" />
+        <Button {...context.args} size="lg" />
+      </div>
+    ),
+  ],
+};
+
+
+export const IconOnly: Story = {
+  args: {
+    variant: 'default',
+    size: 'md',
+    shape: 'normal',
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="space-x-2">
+        <Button
+          {...context.args}
+          size="sm"
+          mode="icon-only"
+          prefixIcon={<User2 />}
+        />
+        <Button
+          {...context.args}
+          size="md"
+          mode="icon-only"
+          prefixIcon={<User2 />}
+        />
+        <Button
+          {...context.args}
+          size="lg"
+          mode="icon-only"
+          prefixIcon={<User2 />}
+        />
+      </div>
+    ),
+  ],
+};
+
+
+
+export const Loading: Story = {
+  args: {
+    variant: 'default',
+    size: 'md',
+    shape: 'normal',
+    loading:true
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="flex items-center space-x-2">
+        <Button
+          {...context.args}
+          size="sm"
+        />
+        <Button
+          {...context.args}
+          size="md"
+        />
+        <Button
+          {...context.args}
+          size="lg"
+        />
+      </div>
+    ),
+  ],
+};
+
+
+export const Disabled: Story = {
+  args: {
+    variant: 'default',
+    size: 'md',
+    shape: 'normal',
+    disabled:true
+  },
+  decorators: [
+    (_story, context) => (
+      <div className="flex items-center space-x-2">
+        <Button
+          {...context.args}
+          size="sm"
+        />
+        <Button
+          {...context.args}
+          size="md"
+        />
+        <Button
+          {...context.args}
+          size="lg"
+        />
+      </div>
+    ),
+  ],
 };
---
+++ src/components/Button/libs/createIcon.ts
@@ -0,0 +1,46 @@
+import type { VariantProps } from 'class-variance-authority';
+import type { ReactNode } from 'react';
+
+import { cn } from '@src/utils/cn';
+import { cva } from 'class-variance-authority';
+import { cloneElement, isValidElement } from 'react';
+
+const classes = cva('shrink-0', {
+  defaultVariants: {
+    size: 'md',
+  },
+  variants: {
+    size: {
+      lg: 'size-6',
+      md: 'size-5',
+      sm: 'size-4 stroke-[2.3px]',
+    },
+  },
+});
+
+export interface CreateIconOptions {
+  className?: string;
+  size?: VariantProps<typeof classes>['size'];
+}
+
+export const createIcon = (
+  node: ReactNode,
+  options: CreateIconOptions = {}
+) => {
+  const { className, size } = options;
+
+  if (isValidElement(node)) {
+    // eslint-disable-next-line @eslint-react/no-clone-element
+    return cloneElement(node, {
+      ...node.props,
+      className: cn(
+        'text-inherit',
+        classes({ size }),
+        className,
+        node.props.className
+      ),
+    });
+  }
+
+  return node;
+};
